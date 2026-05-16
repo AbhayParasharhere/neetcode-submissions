d3164fb28dd00dcc7ab076b35ab46ad62750ab8c
Abhay Parashar <83302622+AbhayParasharhere@users.noreply.github.com>
2026-05-16 07:06:07 -0700

    Add: boats-to-save-people - submission-0

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-0.py
@@ -0,0 +1,28 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        
+        l = 0
+        r = len(people) - 1
+        res = 0
+        peopleSent = 0
+
+        while l <= r and peopleSent < len(people):
+            # Send the most heavy with teh most lighest pair
+            # if tehir sum is more than the llimit - just send the heaviest
+            sum = people[l] + people[r]
+            if sum <= limit:
+                peopleSent -= 2
+                r -= 1
+                l += 1
+                res += 1
+            else:
+                peopleSent -= 1
+                r -= 1
+                res += 1
+        return res
+
+
+
+        
+
