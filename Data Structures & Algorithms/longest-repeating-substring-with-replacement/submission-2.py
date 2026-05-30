4fa0e1569a9e8ace125e7bb4ecb2b3123be9bab0
Abhay Parashar <83302622+AbhayParasharhere@users.noreply.github.com>
2026-05-30 16:25:32 +0530

    Add: longest-repeating-substring-with-replacement - submission-2

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-2.py
@@ -0,0 +1,28 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        l = 0
+        r = 0
+        hmap = [0 for _ in range(26)]
+        maxC = 0
+        maxStr = 0
+        while r < len(s):
+            # put the char in map and record the max count
+            idx = ord(s[r]) - ord('A')
+            hmap[idx] += 1
+            maxC = max(maxC,hmap[idx])
+
+            if (r - l + 1) - maxC > k:
+                # we need to remove the char at l from the map by rediucing count
+                # and update the maxC potentially as well
+                idx2remove = ord(s[l]) - ord('A')
+                hmap[idx2remove] -= 1
+
+                # potentially recaluciate maxC incase its freq count was reduced
+                
+                maxC = max(hmap)
+                
+                l += 1
+            # Update max on every iteration
+            maxStr = max(maxStr,r-l+1)
+            r += 1
+        return maxStr
