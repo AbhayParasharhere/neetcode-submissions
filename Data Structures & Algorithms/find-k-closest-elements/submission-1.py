d594e6883ef6d45f929861a38dc8bbc68bf7c012
Abhay Parashar <83302622+AbhayParasharhere@users.noreply.github.com>
2026-05-31 07:43:26 +0530

    Add: find-k-closest-elements - submission-1

---
+++ Data Structures & Algorithms/find-k-closest-elements/submission-1.py
@@ -0,0 +1,30 @@
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        # BS to find the correct palcement that could have been of the x
+        # then throiugh two pointer grab teh closest k element
+
+        l = 0
+        r = len(arr)
+
+        while l < r:
+            m = (l+r) // 2
+            if arr[m] >= x: r = m
+            else: l = m + 1
+    
+        # Now need to grab k elements 
+        # At every point we compare element at l to elemnt after and before it
+        # out of those 3 the minimum diff to target gets pushed to res
+        # l also moves in taht diretcion
+        lo = l -1
+        hi = l
+        while hi - lo - 1 < k:
+            before = arr[lo] if lo >= 0 else float('infinity')
+            after = arr[hi] if hi < len(arr) else float('infinity')
+
+            if abs(before - x) <= abs(after - x):
+                # move the before pointer
+                lo -= 1
+            else:
+                hi += 1
+
+        return arr[lo+1 : hi]
