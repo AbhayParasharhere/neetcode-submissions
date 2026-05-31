38cb06ece042fc11b97006bb55060d228d3cdbac
Abhay Parashar <83302622+AbhayParasharhere@users.noreply.github.com>
2026-05-31 08:42:19 +0530

    Add: find-in-mountain-array - submission-7

---
+++ Data Structures & Algorithms/find-in-mountain-array/submission-7.py
@@ -0,0 +1,46 @@
+class Solution:
+    def findInMountainArray(self, target: int, mountainArr: 'MountainArray') -> int:
+        # First find the mountain index which acts as a partiton point
+        # then comapre it to target to find the right partion to search
+
+        # Mountain aprtiton first true is such that the element eo the right is less
+        n = mountainArr.length()
+        l = 0
+        r = n
+
+        while l < r:
+            m = (l+r) //2
+            nxt = mountainArr.get(m+1) if m + 1 < n else float('-infinity')
+            at = mountainArr.get(m) if m < n else float('-infinity')
+            if nxt < at: r = m
+            else: l = m + 1
+        
+        mn = l
+        # print(mn)
+
+        def bsAsc(l,r):
+            # print('asc',l,r,mn)
+            bound = r
+            while l < r:
+                m = (l+r) // 2
+                if mountainArr.get(m) >= target: r= m
+                else: l = m + 1
+            if l >= n or mountainArr.get(l) != target: return -1
+            return l
+        
+        def bsDsc(l,r):
+            # print('dsc',l,r)
+            bound = r
+            while l < r:
+                m = (l+r) // 2
+                if mountainArr.get(m) <= target: r = m
+                else: l = m + 1
+            if l >= n or mountainArr.get(l) != target: return -1
+            return l
+        
+        ascRes = bsAsc(0,mn+1)
+        if ascRes == -1:
+            return bsDsc(mn+1,n)
+        else: 
+            return ascRes
+
