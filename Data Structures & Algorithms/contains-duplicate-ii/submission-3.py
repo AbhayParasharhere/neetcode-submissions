7244e538d4e356572344bf3f8caa0c37b7ec34a4
Abhay Parashar <83302622+AbhayParasharhere@users.noreply.github.com>
2026-05-31 08:58:53 +0530

    Add: contains-duplicate-ii - submission-3

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        # Max window size is k
+        # if the element tobe put is within htat window return True
+        # if window size is greater than k then shrink
+        win = set()
+        l = 0
+        if k <= 0: return False
+
+        for r in range(len(nums)):
+            if nums[r] in win:
+                return True
+            
+            if r - l + 1 > k:
+                win.remove(nums[l])
+                l += 1
+            win.add(nums[r])
+        return False
+            
