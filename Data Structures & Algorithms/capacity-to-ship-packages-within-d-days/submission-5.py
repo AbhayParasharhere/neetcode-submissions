505796e223ddb1b17505f8ee0f22726036bc8bdf
Abhay Parashar <83302622+AbhayParasharhere@users.noreply.github.com>
2026-05-31 09:11:58 +0530

    Add: capacity-to-ship-packages-within-d-days - submission-5

---
+++ Data Structures & Algorithms/capacity-to-ship-packages-within-d-days/submission-5.py
@@ -0,0 +1,29 @@
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        # Final wt capacity will be a bianry answer space increasing
+        # bw max of weights and sum of all weights to load
+        # our montonic condition would be can using the m capcity
+        # all packages can be shipped or not - if yes then keep shrinking back
+
+        n = len(weights)
+        l = max(weights)
+        r = sum(weights) + 1
+
+        def canShip(targetMax):
+            # Greedy appoarch ship as much wt possible to target max each day
+            allowedDays = days
+            load = 0
+            for wt in weights:
+                if load + wt > targetMax:
+                    allowedDays -= 1
+                    load = wt
+                else:
+                    load += wt
+            return allowedDays > 0
+
+        while l < r:
+            m = (l+r) // 2
+            if canShip(m): r = m
+            else: l = m + 1
+        
+        return l
