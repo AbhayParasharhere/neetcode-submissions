e8f6b73948c6706b6f9675d6b909e4c0ae0d111a
Abhay Parashar <83302622+AbhayParasharhere@users.noreply.github.com>
2026-06-03 08:37:35 +0530

    Add: asteroid-collision - submission-4

---
+++ Data Structures & Algorithms/asteroid-collision/submission-4.py
@@ -0,0 +1,21 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        res = []
+        stack = deque()
+        incDone = False
+
+        for inc in asteroids:
+            while stack and stack[-1] > 0 and inc < 0:
+                # This is the collision condition
+                # Nowe we see how the collsion resulted
+
+                if abs(inc) > abs(stack[-1]):
+                    stack.pop()
+                else:
+                    if abs(stack[-1]) == abs(inc):
+                        stack.pop()
+                    incDone = True
+                    break
+            if not incDone: stack.append(inc)
+            incDone = False
+        return list(stack)
