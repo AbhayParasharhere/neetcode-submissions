993a1db419661a553237dd5cf2f3c817dd4d301c
Abhay Parashar <83302622+AbhayParasharhere@users.noreply.github.com>
2026-06-08 09:36:34 +0530

    Add: maximum-frequency-stack - submission-5

---
+++ Data Structures & Algorithms/maximum-frequency-stack/submission-5.py
@@ -0,0 +1,40 @@
+class FreqStack:
+
+    def __init__(self):
+        self.hmap = {}
+        self.fmap = {}
+        self.maxF = 0
+
+    def push(self, val: int) -> None:
+        # update the hmap and max
+        self.hmap[val] = self.hmap.get(val,0) + 1
+        f = self.hmap[val]
+        self.maxF = max(self.maxF, f)
+
+        # Now push it to the appropriate fmap stack
+        if f not in self.fmap:
+            self.fmap[f] = deque()
+        self.fmap[f].append(val)
+
+
+    def pop(self) -> int:
+        # we just need to pop from the maxF freq stack
+        val = self.fmap[self.maxF].pop()
+        # remove from the hmap as well
+        self.hmap[val] -= 1
+        if self.hmap[val] <= 0: self.hmap.pop(val)
+
+
+        # in case we reach the end of that max freq stack, move to the prev
+        if not self.fmap[self.maxF]:
+            self.fmap.pop(self.maxF)
+            # update max
+            self.maxF -= 1
+        return val
+        
+
+
+# Your FreqStack object will be instantiated and called as such:
+# obj = FreqStack()
+# obj.push(val)
+# param_2 = obj.pop()
