a5e37e3c63e9e8281b8567ea3f1134fbfd813625
Abhay Parashar <83302622+AbhayParasharhere@users.noreply.github.com>
2026-06-14 10:50:21 +0530

    Add: capacity-to-ship-packages-within-d-days - submission-7

---
+++ Data Structures & Algorithms/capacity-to-ship-packages-within-d-days/submission-7.py
@@ -0,0 +1,32 @@
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        # weight capcity can be sum of all weights which is its right bound here
+        # or it could be the max of the weights otherwise anything less would not be possible
+        # so it forms a natural montonicty of the answers where incresing weight capcity forms a montocity
+        # of wther or not it si even possible to do in the given days to load all wieghts by the belt
+        n = len(weights)
+        r = sum(weights)
+        l = max(weights)
+
+        def possible(wlimit):
+            # we use a greedy apporach
+            # we load as much ina. day as possible
+            # as soon as we exceed capcity we call it a day
+            daysUsed = 1
+            tot = 0
+            for weight in weights:
+                if tot + weight > wlimit:
+                    tot = weight
+                    daysUsed += 1
+                else:
+                    tot += weight
+            return daysUsed <= days
+
+
+        # return first true answer space possible
+        while l < r:
+            m = (l+r) // 2
+            if possible(m): r = m
+            else: l = m + 1
+        
+        return l
