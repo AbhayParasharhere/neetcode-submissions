8db3e9525dd186a4e7a2b3cdb24e65c1343b5c66
Abhay Parashar <83302622+AbhayParasharhere@users.noreply.github.com>
2026-06-16 21:42:24 +0530

    Add: lowest-common-ancestor-in-binary-search-tree - submission-1

---
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-1.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        # approach 1 - cehck all nodes
+        # and the node that satisfies the preoperty being p<=val<=q is teh common ancestor
+        # the first nide that satisfies this condition can set the global variable
+        # any otehr way to have this passed down as teh eventual asnwer??
+        def check(node):
+            if not node: return None
+            lo, hi = min(p.val,q.val), max(p.val,q.val)
+            if node.val >= lo and node.val <= hi:
+                return node
+
+            # recurse on left half and right half if not found
+            lanc = check(node.left)
+            ranc = check(node.right)
+            if lanc or ranc: return lanc or ranc
+        return check(root)
