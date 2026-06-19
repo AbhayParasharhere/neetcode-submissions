98d33328f19e3c95ba27baac3b99ceb976e1419a
Abhay Parashar <83302622+AbhayParasharhere@users.noreply.github.com>
2026-06-19 14:25:56 +0530

    Add: reverse-nodes-in-k-group - submission-3

---
+++ Data Structures & Algorithms/reverse-nodes-in-k-group/submission-3.py
@@ -0,0 +1,52 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        # we have a fx that reverses a list and returns the heead and tattach
+        # then we stich this head and tailn crrectly
+        # we have a counter who is responsibel for calling the fx every k step 
+        # if at the end counter is not a multiple of k we dont call this helps us aoid the last chain which might not split evenly into k parts as we dont have to reverse it
+
+        def rev_part(node):
+            i = 0
+            t = node
+            prev = None
+            while node and i < k:
+                temp = node.next
+                node.next = prev
+                prev = node
+                node = temp
+                i += 1
+            return (prev,t)
+        
+        cur = 0
+        dummy = ListNode(0)
+        dummy.next = head
+        hattach = dummy
+        tattach = head
+
+        while tattach.next:
+            cur += 1
+            tattach = tattach.next
+            if cur == k:
+                h,t = rev_part(hattach.next)
+                t.next = tattach
+                hattach.next = h
+
+                # now move hattach forward k times for the next iteration
+                for r in range(k):
+                    if hattach: hattach = hattach.next
+                cur = 0
+        if cur + 1 == k:
+            # the last group is complete (cur counted + 1 for tattach itself)
+            h, t = rev_part(hattach.next)
+            t.next = None       # tattach was the last node, so nothing after
+            hattach.next = h
+        return dummy.next
+
+
+
