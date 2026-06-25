c11f9ec355294dce79085d29820465a005fa3d20
Abhay Parashar <83302622+AbhayParasharhere@users.noreply.github.com>
2026-06-25 11:22:25 +0530

    Add: merge-k-sorted-linked-lists - submission-4

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-4.py
@@ -0,0 +1,40 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:    
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        # we mege two lists at the l and r pointer and put the result in the l location
+        # we complete half the list when l becomes > r
+        # we continue this logratihmic division until list is singular
+        # every l pos in the list contributes or is merged only logn times
+        # so complex is logn 
+        if not lists: return None
+        def mergeTwo(l1,l2):
+            res = ListNode(0)
+            h = res
+            while l1 and l2:
+                if l1.val <= l2.val:
+                    res.next = l1
+                    l1 = l1.next
+                else:
+                    res.next = l2
+                    l2 = l2.next
+                res = res.next
+            # stich the remainder
+            res.next = l1 or l2
+            return h
+
+        n = len(lists)
+        # continue until our halving reaches a singular list
+        while n > 1:
+            l = 0
+            r = n -1
+            while l < r:
+                lists[l] = mergeTwo(lists[l],lists[r]).next
+                l += 1
+                r -= 1
+            n = math.ceil(n/2)
+        return lists[0]
