5241647c3393cc583ff3872182f5189ca378ddcf
Abhay Parashar <83302622+AbhayParasharhere@users.noreply.github.com>
2026-08-11 10:40:53 +0530

    Add: combination-target-sum-ii - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-0.py
@@ -0,0 +1,24 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        # first lets see what happens if we dont handle duplicates specially
+        res = []
+        n = len(candidates)
+        candidates.sort()
+        def backtrack(i,comb,sum_left):
+            if sum_left == 0:
+                res.append(comb[:])
+                return
+            if i >= n: return
+            elif sum_left < 0:
+                return
+            val_used = set()
+            for j in range(i,n):
+                if candidates[j] in val_used: continue
+                # each number atmost once so j + 1
+                comb.append(candidates[j])
+                val_used.add(candidates[j])
+                backtrack(j+1,comb,sum_left - candidates[j])
+                comb.pop()
+        backtrack(0,[],target)
+        # print(res)
+        return res
