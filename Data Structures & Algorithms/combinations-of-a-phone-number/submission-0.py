864c7be21c614a75a6a35d24332b1161ba6d929c
Abhay Parashar <83302622+AbhayParasharhere@users.noreply.github.com>
2026-08-12 10:29:53 +0530

    Add: combinations-of-a-phone-number - submission-0

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-0.py
@@ -0,0 +1,30 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        hmap = {
+            2:["a","b","c"],
+            3:["d","e","f"],
+            4:["g","h","i"],
+            5:["j","k","l"],
+            6:["m","n","o"],
+            7:["p","q","r","s"],
+            8:["t","u","v"],
+            9:["w","x","y","z"]
+        }
+        n = len(digits)
+        res = []
+        if not digits: return []
+        def backtrack(i,path):
+            if i == n and len(path) == n:
+                res.append("".join(path))
+                return
+            
+            # we dont need forward metaching with already decided cahr
+            # so i to n not 0 to n
+            for j in range(i,n):
+                for choice in hmap[int(digits[j])]:
+                    # print(path)
+                    path.append(choice)
+                    backtrack(j+1,path)
+                    path.pop()
+        backtrack(0,[])
+        return res
