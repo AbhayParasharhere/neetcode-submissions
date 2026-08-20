9b0c2d7b0e0b9eb089c6d76b4b695a8685f665a7
Abhay Parashar <83302622+AbhayParasharhere@users.noreply.github.com>
2026-08-20 22:49:02 +0530

    Add: coin-change-ii - submission-1

---
+++ Data Structures & Algorithms/coin-change-ii/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        # combination question with repeat allowed - perm will giev us duplicate leaves
+        # for i,j and j,i both hence we will overcount
+        # we need to count branches where we have 1
+
+        cache = {}
+        n = len(coins)
+        def solve(i,left):
+            if (i,left) in cache: return cache[(i,left)]
+            if left == 0:
+                return 1
+            if i>=n or left < 0:
+                return 0
+            res = 0
+            skip = solve(i+1,left)
+            take = solve(i,left-coins[i])
+            res += skip + take
+            cache[(i,left)] = res
+            return res
+        return solve(0,amount)
